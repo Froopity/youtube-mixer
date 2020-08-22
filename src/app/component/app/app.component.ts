89b3b6d8cc5aad5a0548849983540c05c6a9d669
Froopity <70052766+Froopity@users.noreply.github.com>
2020-08-22 17:54:09 +0800

    mixer-2 improve: Video component (#6)
    * Moved video selector into video component
    
    * Added pretty styling
    
    * Allowed entering of entire urls
    
    Co-authored-by: William <william@kulin.com.au>

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -9,22 +9,4 @@ import {BehaviorSubject} from 'rxjs';
 })
 export class AppComponent {
   title = 'YouTube Mixer';
-
-  readonly form: FormGroup;
-
-  private readonly id: AbstractControl;
-
-  videoId$ = new BehaviorSubject<string>('oHg5SJYRHA0');
-
-  constructor(formBuilder: FormBuilder) {
-    this.form = formBuilder.group({
-      id: ''
-    });
-
-    this.id = this.form.get('id');
-  }
-
-  onSubmit(): void {
-    this.videoId$.next(this.id.value);
-  }
 }
--- src/app/component/youtube-video/youtube-video.component.ts
+++ src/app/component/youtube-video/youtube-video.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-youtube-video',
@@ -6,15 +8,30 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./youtube-video.component.scss']
 })
 export class YoutubeVideoComponent implements OnInit {
-  @Input()
-  readonly videoId: string;
+  readonly form: FormGroup;
 
-  constructor() {
+  private readonly id: AbstractControl;
+
+  videoId$ = new BehaviorSubject<string>('oHg5SJYRHA0');
+
+  constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      id: ''
+    });
+
+    this.id = this.form.get('id');
   }
 
   ngOnInit(): void {
     const tag = document.createElement('script');
     tag.src = 'http://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
+  }
+
+  onSubmit(): void {
+    const value = this.id.value as string;
+    const values = value.split('=');
+
+    this.videoId$.next(values[values.length - 1]);
   }
 }
