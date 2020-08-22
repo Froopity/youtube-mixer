28056a9b35463ebf1e8dcd7c0c6ef8dd7e8314c6
William <william@kulin.com.au>
2020-08-22 13:42:06 +0800

    Added input field and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './component/app/app.component';
-import { YoutubeVideoComponent } from './component/youtube-video/youtube-video.component';
+import {AppComponent} from './component/app/app.component';
+import {YoutubeVideoComponent} from './component/youtube-video/youtube-video.component';
 import {YouTubePlayerModule} from '@angular/youtube-player';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,15 @@ import {YouTubePlayerModule} from '@angular/youtube-player';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
     YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yt-mixer';
+  title = 'YouTube Mixer';
+
+  private readonly form: FormGroup;
+
+  private readonly id: AbstractControl;
+
+  videoId$ = new BehaviorSubject<string>('oHg5SJYRHA0');
+
+  constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      id: ''
+    });
+
+    this.id = this.form.get('id');
+  }
+
+  onSubmit(): void {
+    this.videoId$.next(this.id.value);
+  }
 }
