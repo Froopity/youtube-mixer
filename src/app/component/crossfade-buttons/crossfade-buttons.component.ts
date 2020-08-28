21baf3bb07d035e9c3321c91e62acc329b323d80
Froopity <70052766+Froopity@users.noreply.github.com>
2020-08-28 23:56:05 +0800

    Mixer-21 improve: Crossfade button enhancement and positioning (#22)
    * All buttons added and styling when clicked, crossfade only on crossfade
    
    * Shuffled button positions
    
    Co-authored-by: William <william@kulin.com.au>

--- src/app/component/crossfade-buttons/crossfade-buttons.component.ts
+++ src/app/component/crossfade-buttons/crossfade-buttons.component.ts
@@ -3,16 +3,34 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-crossfade-buttons',
   templateUrl: './crossfade-buttons.component.html',
-  styleUrls: ['./crossfade-buttons.component.css']
+  styleUrls: ['./crossfade-buttons.component.scss']
 })
 export class CrossfadeButtonsComponent implements OnInit {
 
   @Input()
-  readonly func: (over: number) => void;
+  readonly crossfade: (over: number) => void;
 
-  constructor() { }
+  @Input()
+  readonly durations = [5, 10, 30, 60];
+
+  selectedDuration: number;
+
+  constructor() {
+    this.selectedDuration = this.durations[0];
+  }
 
   ngOnInit(): void {
   }
 
+  click(duration: number): void {
+    this.selectedDuration = duration;
+  }
+
+  isSelected(duration: number): boolean {
+    return duration === this.selectedDuration;
+  }
+
+  fade(): void {
+    this.crossfade(this.selectedDuration);
+  }
 }
